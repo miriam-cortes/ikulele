4608b461a2c5b328ca0f51e8eefd90f049be1615
Miriam Cortes <mcortes.umiami@gmail.com>
2017-01-06 14:11:52 -0800

    temporary index pg for heroku

---
+++ app/controllers/homepages_controller.rb
@@ -0,0 +1,25 @@
+class HomepagesController < ApplicationController
+  def index
+  end
+
+  def search_results
+  end
+
+  def song
+  end
+
+  def artist
+  end
+
+  def album
+  end
+
+  def random
+  end
+
+  def chord_bank
+  end
+
+  def favorites
+  end
+end
