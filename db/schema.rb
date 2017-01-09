04c5c9a2585afee32fee99b4399fd48cc31c7d65
Miriam Cortes <mcortes.umiami@gmail.com>
2017-01-08 19:42:49 -0800

    added column in users for favorite songs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170108232102) do
+ActiveRecord::Schema.define(version: 20170109032401) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170108232102) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "favorite_songs"
   end
 
 end
