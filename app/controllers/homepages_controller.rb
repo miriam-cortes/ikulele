f5d634ccbb49515eed6c4bd21a65068c74fc68ca
Miriam Cortes <mcortes.umiami@gmail.com>
2017-01-11 16:43:52 -0800

    can search through songs and artists

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -5,8 +5,10 @@ class HomepagesController < ApplicationController
   def search_results
     @search_term = params["search"]
     # raise
+    @song_search_results = Song.where("name like ?", "%#{@search_term}%")
+    @artist_search_results = Artist.where("name like ?", "%#{@search_term}%")
 
-    return @search_term
+    return @search_term, @song_search_results, @artist_search_results
   end
 
   def song
