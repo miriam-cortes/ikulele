8a6ccdbceee7a5ce15dd14b37ce4186abbe38d0e
Miriam Cortes <mcortes.umiami@gmail.com>
2017-01-23 15:13:26 -0800

    routes to fb login

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get 'homepages/favorites' => 'homepages#favorites', as: 'favorites'
   post 'sessions/favorites' => 'sessions#handle_favorite', as: 'handle_favorite'
 
-  match 'auth/:provider/callback', to: 'sessions#create',as: 'signin', via: [:get, :post]
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
