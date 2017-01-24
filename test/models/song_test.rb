b10de9398b75bd4e110e1abd2a37c96ef1b87b2d
Miriam Cortes <mcortes.umiami@gmail.com>
2017-01-24 13:55:52 -0800

    added Travis CI

--- test/models/song_test.rb
+++ test/models/song_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class SongTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  #   assert_template :index
+  # end
+  #
+  # test "should get show" do
+  #   get :show, {id: categories(:one).id}
+  #   assert_response :success
+  #   assert_template :show
+  # end
+  #
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
+  #
+  # test "should be able to create a new category" do
+  #   post_params = {category: {name: "Harry Potter Things"}}
+  #   post :create, post_params
+  #   assert_response :redirect
   # end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,27 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+require "simplecov"
+SimpleCov.start
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  Minitest::Reporters.use!
+
+  def setup
+    # Once you have enabled test mode, all requests
+    # to OmniAuth will be short circuited to use the mock authentication hash.
+    # A request to /auth/provider will redirect immediately to /auth/provider/callback.
+    OmniAuth.config.test_mode = true
+
+    # The mock_auth configuration allows you to set per-provider (or default) authentication
+    # hashes to return during testing.
+    OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+      provider: 'facebook', uid: '123545', info: { email: "a@b.com", name: "Ada" }
+    })
+  end
 
   # Add more helper methods to be used by all tests here...
 end
