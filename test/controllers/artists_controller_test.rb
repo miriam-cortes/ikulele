1a019f2180442b086857b68df63e45272e1c597f
Miriam Cortes <mcortes.umiami@gmail.com>
2017-01-25 15:53:45 -0800

    trying out with no artist create in controller

--- test/controllers/artists_controller_test.rb
+++ test/controllers/artists_controller_test.rb
@@ -6,20 +6,20 @@ class ArtistsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get show" do
+    get :show, {id: artists(:artistOne).id}
+    assert_response :success
+  end
+
   test "should get new" do
     get :new
     assert_response :success
   end
 
   # test "should get create" do
-  #   get :create, {id: artists(:artistOne)}
+  #   get :create, {id: 4}
   #   assert_response :success
   # end
 
-  # test "should get show_artist_songs" do
-  #   param = {id: artists(:artistOne).id}
-  #   get :show_artist_songs, param
-  #   assert_response :success
-  # end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,7 +19,9 @@ class ActiveSupport::TestCase
     # The mock_auth configuration allows you to set per-provider (or default) authentication
     # hashes to return during testing.
     OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
-      provider: 'facebook', uid: '123545', info: { email: "a@b.com", name: "Ada" }
+      provider: 'facebook',
+      uid: '123545',
+      info: { email: "a@b.com", name: "Ada" }
     })
   end
 
