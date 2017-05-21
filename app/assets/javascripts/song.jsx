cb45e1bcb0742e5e5b041a4d8b1eb563b0ef8f57
Miriam Cortes <mcortes.umiami@gmail.com>
2017-05-20 20:58:55 -0700

    removed signin and updated homepage

---
+++ app/assets/javascripts/song.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Song extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>What is important now is to recover our senses.</p>
+      </div>
+    );
+  }
+};
